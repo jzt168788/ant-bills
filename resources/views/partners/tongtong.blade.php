049fe868ba764649f6fec39d3062e561c65d35ee
Jinzhuotao <jin.zhuotao@9douyu.com>
2018-04-09 10:40:19 +0800

    guanghui

---
+++ resources/views/partners/tongtong.blade.php
@@ -0,0 +1,50 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title>女王彤彤</title>
+
+    <link href="https://fonts.googleapis.com/css?family=Lato:100" rel="stylesheet" type="text/css">
+
+    <style>
+        html, body {
+            height: 100%;
+        }
+
+        body {
+            margin: 0;
+            padding: 0;
+            width: 100%;
+            color: #B0BEC5;
+            display: table;
+            font-weight: 100;
+            font-family: 'Lato';
+        }
+
+        .container {
+            text-align: center;
+            display: table-cell;
+            vertical-align: middle;
+        }
+
+        .content {
+            text-align: center;
+            display: inline-block;
+        }
+
+        .title {
+            font-size: 35px;
+            margin-bottom: 40px;
+        }
+    </style>
+</head>
+<body>
+<div class="container">
+    <div class="content">
+        <h1>王彤彤你这个臭局局.</h1>
+        <h3>母脑斧.</h3>
+        <h2>你算哪块小饼干！</h2>
+        <h4>你这个笨蛋蛋.</h4>
+    </div>
+</div>
+</body>
+</html>
